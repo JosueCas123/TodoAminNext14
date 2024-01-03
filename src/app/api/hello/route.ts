bc67bfef3068a730c1c6d4e8759195d0018c4860
Josue Castillo <isaac9900.jc@gmail.com>
2024-01-03 13:00:56 -0400

    todoAdmin inicio

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+    return NextResponse.json({
+        message: 'Hello World'
+    })
+
+ 
+}
